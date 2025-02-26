e8f5b847c1ec95cabedf03d211e272d6c17556f2
Marius Vikhammer <marius.vikhammer@espressif.com>
2025-02-26 10:21:03 +0800

    Put main CPU to sleep and wake up from ULP

--- main/my_ulp_app.c
+++ main/my_ulp_app.c
@@ -3,12 +3,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "ulp_hello_world.h"
+#include "esp_sleep.h"
 
 extern const uint8_t ulp_hello_world_bin_start[] asm("_binary_ulp_hello_world_bin_start");
 extern const uint8_t ulp_hello_world_bin_end[]   asm("_binary_ulp_hello_world_bin_end");
 
 void app_main(void)
 {
+    if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_ULP) {
+        printf("Wake-up cause was: ESP_SLEEP_WAKEUP_ULP\n");
+    }
+
     ulp_lp_core_cfg_t cfg = {
         .wakeup_source = ULP_LP_CORE_WAKEUP_SOURCE_LP_TIMER,
         .lp_timer_sleep_duration_us = 1000000,
@@ -19,8 +24,9 @@ void app_main(void)
 
     printf("LP core loaded with firmware and running successfully\n");
 
-    while(1) {
-        printf("ulp counter: %lu\n", ulp_my_counter);
-        vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+    /* Enable the ULP to wakeup the rest of the system */
+    ESP_ERROR_CHECK(esp_sleep_enable_ulp_wakeup());
+
+    // Go into to deep sleep
+    esp_deep_sleep_start();
 }
--- main/ulp_app/ulp_main.c
+++ main/ulp_app/ulp_main.c
@@ -4,9 +4,15 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
+#include "ulp_lp_core_utils.h"
+
  int my_counter = 0;
 
  int main (void)
  {
      my_counter++;
+
+     if (my_counter >= 5) {
+        ulp_lp_core_wakeup_main_processor();
+    }
  }
